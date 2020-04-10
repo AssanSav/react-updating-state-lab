d3a42bbd4f198015154a1a043776d2947f426e7c
AssanSav <assane.savadogo81@gmail.com>
2020-04-09 23:49:44 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
-// Code DigitalClicker Component Here
+import React, { Component } from "react"
+
+export default class DigitalClicker extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+        this.simulate = this.simulate.bind(this)
+    }
+
+    simulate() {
+        this.setState(previousState => {
+            return {
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.simulate}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,54 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from "react"
+
+export default class YouTubeDebugger extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: "1080p"
+                }
+            }
+        }
+        this.handleBitrateOnClick = this.handleBitrateOnClick.bind(this)
+        this.handleResolutionWhenClicked = this.handleResolutionWhenClicked.bind(this)
+    }
+
+    handleBitrateOnClick() {
+        this.setState(previousState => {
+            return {
+                settings: {
+                    ...this.state.settings,
+                    bitrate: 12
+                }
+            }
+        })
+    }
+
+    handleResolutionWhenClicked() {
+        this.setState(previousState => {
+            return {
+                settings: {
+                    ...this.state.settings,
+                    video: {
+                        ...this.state.video,
+                        resolution: "720p"
+                    }
+                }
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className='bitrate' onClick={this.handleBitrateOnClick}>Click Me To Update the Bitrate!</button>
+                <button className='resolution' onClick={this.handleResolutionWhenClicked}>Click Me To Update the Resolution</button>
+            </div>
+        )
+    }
+}
